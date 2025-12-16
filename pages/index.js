16c42ef0380fc21c57ccac59bd30fba046599499
Chiragcs8 <chirag98589@gmail.com>
2025-12-17 01:54:03 +0530

    Refactor components: Update Button, Footer, Header, Loader, and index with improved structure and functionality

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,106 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import toast from "react-hot-toast";
+
+//INTERNAL IMPORT
+import { useStateContext } from "../Context/index";
+import Header from "../Components/Header";
+import Input from "../Components/Input";
+import Button from "../Components/Button";
+import Table from "../Components/Table";
+import PreSaleList from "../Components/PreSaleList";
+import UploadLogo from "../Components/UploadLogo";
+import Loader from "../Components/Loader";
+import Footer from "../Components/Footer";
+import ICOMarket from "../Components/ICOMarket";
+import TokenCreator from "../Components/TokenCreator";
+import TokenHistory from "../Components/TokenHistory";
+import Marketplace from "../Components/Marketplace";
+import CreateICO from "../Components/CreateICO";
+import Card from "../Components/Card";
+import BuyToken from "../Components/BuyToken";
+import WidthdrawToken from "../Components/WidthdrawToken";
+import TokenTransfer from "../Components/TokenTransfer";
 
 const index = () => {
-  return <div>
-    <h1>
-      ICO Marketplace
-    </h1>
-  </div>;
+  const {
+    widthdrawToken,
+    transferTokens,
+    buyToken,
+    createICOSALE,
+    GET_ALL_USER_ICOSALE_TOKEN,
+    GET_ALL_ICOSALE_TOKEN,
+    createERC20,
+    connectWallet,
+    openBuyToken,
+    setOpenBuyToken,
+    openWidthdrawToken,
+    setOpenWidthdrawToken,
+    openTransferToken,
+    setOpenTransferToken,
+    openTokenCreator,
+    setOpenTokenCreator,
+    openCreateICO,
+    setOpenCreateICO,
+    address,
+    setAddress,
+    accountBalance,
+    loader,
+    setLoader,
+    currency,
+    PINATA_AIP_KEY,
+    PINATA_SECRECT_KEY,
+    ICO_MARKETPLACE_ADDRESS,
+    shortenAddress,
+  } = useStateContext();
+
+  const notifySuccess = (msg) => toast.success(msg, { duration: 200 });
+  const notifyError = (msg) => toast.error(msg, { duration: 200 });
+
+  const [allICOs, setAllICOs] = useState();
+  const [allUSerICOs, setAllUserICOs] = useState();
+
+  const [openAllICO, setOpenAllICO] = useState(false);
+  const [openTokenHistory, setOpenTokenHistory] = useState(false);
+  const [openICOMarketplace, setOpenICOMarketplace] = useState(false);
+
+  //BUY ICO TOKEN
+  const [buyICO, setBuyICO] = useState();
+
+  const copyAddress = () => {
+    navigator.clipboard.writeText(ICO_MARKETPLACE_ADDRESS);
+    notifySuccess("Copid successfully");
+  };
+
+  return (
+    <div>
+      <Header
+        accountBalance={accountBalance}
+        setAddress={setAddress}
+        address={address}
+        connectWallet={connectWallet}
+        ICO_MARKETPLACE_ADDRESS={ICO_MARKETPLACE_ADDRESS}
+        shortenAddress={shortenAddress}
+        setOpenAllICO={setOpenAllICO}
+        openAllICO={openAllICO}
+        setOpenTokenCreator={setOpenTokenCreator}
+        openTokenCreator={openTokenCreator}
+        setOpenTokenHistory={setOpenTokenHistory}
+        openTokenHistory={openTokenHistory}
+        setOpenICOMarketplace={setOpenICOMarketplace}
+        openICOMarketplace={openICOMarketplace}
+      />
+      {openAllICO && <ICOMarket />}
+      {openTokenCreator && <TokenCreator />}
+      {openTokenHistory && <CreateICO />}
+      {openICOMarketplace && <ICOMarket />}
+      {openBuyToken && <BuyToken />}
+      {openTransferToken && <TokenTransfer />}
+      {openWidthdrawToken && <WidthdrawToken />}
+
+      <Footer />
+      {loader && <Loader />}
+    </div>
+  );
 };
 
 export default index;
